290f6956647345009f2ccf0adb8e80114749dc5c
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-20 22:21:31 -0700

    adds sign up form page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to root_path, :notice => "Signed up!"
+		else
+			## what's this?
+			render "new"
+		end
+	end
+
 end
