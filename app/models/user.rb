8f8b4dfe3fae26ab5de01c3fc08926fba18843ca
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-20 22:03:43 -0700

    adds sites controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	## validations go here
+
 	def self.authenticate email, password 
 		User.find_by_email(email).try(:authenticate, password)
 	end
+
+	## set password reset goes here
 
 end
