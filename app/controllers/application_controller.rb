f3649e71c13ab2d4f310450fbdc8de6624ca503c
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-21 12:15:20 -0700

    adds auth and @current_user methods to application and sites controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def is_authenticated?
+  	redirect_to login_url unless current_user
+  end
+
+  def current_user
+  	@current_user ||= User.find_by(id: session[:user_id])
+
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,10 +1,10 @@
 class SitesController < ApplicationController
 
 	before_action :is_authenticated?
-	before_filter :is_authenticated?, :except => :index
+	# before_filter :is_authenticated?, :except => :index
 
 	def index
-
+		render text: "Hi #{current_user.email}!! This note is in the Site controller"
 		
 	end
 
