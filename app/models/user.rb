e432fc2de8740d729ff585bcf4d6f67771425cb3
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-22 22:02:42 -0700

    adds student model, associates with user in simple way

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
-	has_many :polls
+	has_many :polls, :students
 
 	## validations go here
 	## regex, only letters in name?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923025143) do
+ActiveRecord::Schema.define(version: 20140923045748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20140923025143) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "from_number"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "sms_phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
