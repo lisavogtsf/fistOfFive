928197c90a124f4e5d2c1fc08731f4bda920c69a
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-25 10:58:09 -0700

    removes binding.pry

--- app/controllers/sendtexts_controller.rb
+++ app/controllers/sendtexts_controller.rb
@@ -33,7 +33,7 @@ class SendtextsController < ApplicationController
 		## mark poll as sent and open
 		@poll.time_sent = Time.now
 		@poll.is_open = true
-		binding.pry
+
 		# redirect_to user_poll_path(@user.id, @poll.id)
 		redirect_to user_poll_path(@poll.user_id, @poll.id), :notice => "poll sent, wait for replies then refresh your page"
 
