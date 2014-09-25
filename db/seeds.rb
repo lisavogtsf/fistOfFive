c11954ed3549fd967c5c90b3eeff811ceb744bd0
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-25 06:57:44 -0700

    adds some student seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+taco = Student.new
+taco.first_name = "Taco"
+taco.sms_phone_number = "4158675309"
+taco.email = "taco@generalasssemb.ly"
+taco.save
+
+taco = Student.new
+taco.first_name = "Nacho"
+taco.sms_phone_number = "4158675310"
+taco.email = "nacho@generalasssemb.ly"
+taco.save
+
+taco = Student.new
+taco.first_name = "Han"
+taco.last_name = "Solo"
+taco.sms_phone_number = "4158675311"
+taco.email = "han@generalasssemb.ly"
+taco.save
