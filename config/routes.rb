ef09aec37d1845e2fd9163da739ef9996b9054d3
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-24 23:15:17 -0700

    tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
 
   post 'send_sms' => 'sendtexts#send_text_message'
+  # get 'send_sms' => 'sendtexts#send_text_message'
   post 'sms' => 'receive_messages#index'
   # ## testing
   # get 'sms' => 'receive_messages#index'
