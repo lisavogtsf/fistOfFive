316e4172e03c7d71ca08e94bc93f7f976859065c
Lisa Vogt <lisavogt.sf@gmail.com>
2014-09-28 21:10:59 -0700

    adds user email as author, website URL

--- app/controllers/sendtexts_controller.rb
+++ app/controllers/sendtexts_controller.rb
@@ -6,14 +6,16 @@ class SendtextsController < ApplicationController
 
 	def send_text_message
 
-		numbers_for_recipients = ["4159671051", "4158465918"]
+		# need to fix to allow other recipients
+		numbers_for_recipients = [ENV['GOOGLE_VOICE_NUMBER']]
 
-
-		# @user = User.find(params[:user])
 		@poll = Poll.find_by_id(params[:id])
+		@user = User.find_by_id(@poll.user_id)
 		message_content = @poll.topic
-		message_template = "Please reply with feedback on: "
-		message_scale = " (scale of 0-5, then optional question or comment)"
+		message_template = "Reply with feedback on: "
+		message_scale = " (scale of 0-5, with optional question/comment)."
+		message_author = " Sent by " + (@user.email)
+		message_site = " using http://fistof5.herokuapp.com"
 
 		twilio_sid = ENV['TWILIO_ACCOUNT_SID']
 		twilio_token = ENV['TWILIO_AUTH_TOKEN']
@@ -28,7 +30,7 @@ class SendtextsController < ApplicationController
 			@twilio_client.account.sms.messages.create(
 				:from => "+1#{twilio_phone_number}",
 				:to => "+1#{formatted_num}",
-				:body => message_template + message_content + message_scale
+				:body => message_template + message_content + message_scale + message_author + message_site
 				)
 		end
 
@@ -42,5 +44,10 @@ class SendtextsController < ApplicationController
 		# redirect_to user_poll_path(@user.id, @poll.id), :notice => "message sent"
 	end
 
+	def send_admin_alert
+		twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+		twilio_token = ENV['TWILIO_AUTH_TOKEN']
+		twilio_phone_number = ENV['TWILIO_NUMBER']
+	end
 
 end
