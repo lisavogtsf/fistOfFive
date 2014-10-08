3cb6a4cf3e43db61cd1b5b1a7465dbca6eeabc3d
Lisa Vogt <lisavogt.sf@gmail.com>
2014-10-08 12:24:46 -0700

    removes old admins table

---
+++ db/migrate/20141008192347_remove_admins_table.rb
@@ -0,0 +1,5 @@
+class RemoveAdminsTable < ActiveRecord::Migration
+  def change
+  	drop_table :admins
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008192129) do
+ActiveRecord::Schema.define(version: 20141008192347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "affiliations", force: true do |t|
     t.integer  "user_id"
