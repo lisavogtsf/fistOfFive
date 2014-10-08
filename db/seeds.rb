064037ed678aaff65d0e1ee62749f20e8172a064
Lisa Vogt <lisavogt.sf@gmail.com>
2014-10-08 12:47:59 -0700

    adds seeds for new configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-taco = Student.new
-taco.first_name = "Taco"
-taco.sms_phone_number = "4158675309"
-taco.email = "taco@generalasssemb.ly"
-taco.save
 
-taco = Student.new
-taco.first_name = "Nacho"
-taco.sms_phone_number = "4158675310"
-taco.email = "nacho@generalasssemb.ly"
-taco.save
+## sample user for fistOfFive
+# turing = User.create(first_name: 'Alan', last_name: 'Turing', email: 'alan@alan.com', password: '4321', password_confirmation: '4321', sms_phone_number: '4154448888')
 
-taco = Student.new
-taco.first_name = "Han"
-taco.last_name = "Solo"
-taco.sms_phone_number = "4158675311"
-taco.email = "han@generalasssemb.ly"
-taco.save
+# nachito = User.create(first_name: 'Ignacio', last_name: 'Iglesias', email: 'nacho@nacho.com', password: '4321', password_confirmation: '4321', sms_phone_number: '4158888888')
+
+# uxdi = Course.create(title: 'UXDI', desc: 'User experience design immersive', schedule: 'SF-July')
+
+# nachito = User.find_by_email('nacho@nacho.com') 
+# turing = User.find_by_email('alan@alan.com')
+
+# wdi = Course.find_by_title('WDI')
+# uxdi = Course.find_by_title('UXDI')
+
+# nachito.courses << uxdi
+# turing.courses << uxdi 
+
+
+# taco = Student.new
+# taco.first_name = "Taco"
+# taco.sms_phone_number = "4158675309"
+# taco.email = "taco@generalasssemb.ly"
+# taco.save
+
+# taco = Student.new
+# taco.first_name = "Nacho"
+# taco.sms_phone_number = "4158675310"
+# taco.email = "nacho@generalasssemb.ly"
+# taco.save
+
+# taco = Student.new
+# taco.first_name = "Han"
+# taco.last_name = "Solo"
+# taco.sms_phone_number = "4158675311"
+# taco.email = "han@generalasssemb.ly"
+# taco.save
