9d543b87497bcbb83b4842271cb93f821f0638fc
Lisa Vogt <lisavogt.sf@gmail.com>
2014-10-08 10:26:44 -0700

    removes old jquery from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-1.7.2.min
 //= require jquery_ujs
 //= require bootstrap-3.2.0.min
 //= require_tree .
