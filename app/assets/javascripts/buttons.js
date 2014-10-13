f67c0d978add37fdc386e192ee865f5b5c663007
Lisa Vogt <lisavogt.sf@gmail.com>
2014-10-12 21:28:29 -0700

    adds jquery to add confirm delete dialogue

---
+++ app/assets/javascripts/buttons.js
@@ -0,0 +1,14 @@
+// buttons.js
+
+// $('button[name="delete"]').on('click', function(e){
+// 	var $form=$(this).closest('form');
+// 	e.preventDefault();
+// 	$('#confirm').modal({backdrop: 'static', keyboard:false })
+// 		.one('click', '#delete', function(){
+// 			$form.trigger('submit'); // submit the form
+// 		});
+// });
+
+	$(document).ready(function (){
+		console.log('ready!')
+	});
