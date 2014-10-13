fd7576f5cf99c0334a3528e438e516ad56da6dd4
Lisa Vogt <lisavogt.sf@gmail.com>
2014-10-12 22:54:19 -0700

    validates that messages are from app user

--- app/controllers/receive_messages_controller.rb
+++ app/controllers/receive_messages_controller.rb
@@ -15,12 +15,16 @@ class ReceiveMessagesController < ApplicationController
 		message_body = params["Body"]
 		## accept body of texted reply
 		from_number = params["From"]
+		## need to use this From to find the user_id of who sent it
+		## if the sender is not a user of the app, the reply won't be accepted
+		sms_user = User.find_by_sms_phone_number(from_number)
 		
 		## determine the most recently sent poll
 		## if successful proceed
-		@poll = Poll.order(:time_sent).last
+		#@poll = Poll.order(:time_sent).last
+		@poll = Poll.where(is_open: true).order(:time_sent).last
 		binding.pry
-		if @poll 
+		if @poll && sms_user
 			## and info on poll -- what poll is open?
 			## for testing
 			#@poll = Poll.find_by_id(17)
@@ -29,14 +33,16 @@ class ReceiveMessagesController < ApplicationController
 			@reply.response = message_body
 			@reply.from_number = from_number
 			@reply.poll_id = @poll.id
-			@reply.user_id = @poll.user_id		
+			@reply.user_id = sms_user.id		
 			@poll.replies << @reply
 
 			@reply.save
 				
 			render :nothing => true, :status => 200, :content_type => 'text/html'
-		## if a poll wasn't found, then they're not replying about anything
+		else
+		## if a poll/user wasn't found, then they're not replying about anything
 		## just ignore for now, Twilio will log it?
+			render :nothing => true, :status => 200, :content_type => 'text/html'
 		end
 	end
 
